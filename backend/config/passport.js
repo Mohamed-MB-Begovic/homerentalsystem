2d81b6693ce4271a34c005d1c8574917403ff82c
Mohamed-MB-Begovic <mohamedmohamudabdulahiabdi@gmail.com>
2025-06-23 12:22:39 -0700

    Update passport.js

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -11,7 +11,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:9000/auth/google/callback",
+      callbackURL: "https://homerentalsystem-hsvi.onrender.com/auth/google/callback",
       scope: ["profile", "email"],
     },
     async(accessToken, refreshToken, profile, done) => {
