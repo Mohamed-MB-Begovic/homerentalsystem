0554850e246d66b23afe6e83356eff981b8f8a40
Mohamed-MB-Begovic <mohamedmohamudabdulahiabdi@gmail.com>
2025-06-23 21:46:07 -0700

    Update Header.jsx

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -339,7 +339,9 @@ const logout = async () => {
 
                     <button 
                       className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition"
-                      onClick={() => { logOut(); setMobileMenuOpen(false); }}
+                      onClick={()=>{
+                      logout()
+                       setMobileMenuOpen(false)}}
                     >
                       Logout
                     </button>
